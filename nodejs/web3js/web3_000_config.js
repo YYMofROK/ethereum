8d7457119d0cb198434d32070d762c1c06f6971b
YYMofROK <a84146943@gmail.com>
2020-10-26 14:38:48 +0900

    Create web3_000_config.js

---
+++ nodejs/web3js/web3_000_config.js
@@ -0,0 +1,10 @@
+
+
+
+
+module.exports = {
+		getNodeAddress_default : function() {  return "http://192.168.0.40:80" },
+//		getNodeAddress_default : function() {  return "https://mainnet.infura.io:443/v3/394a621745b743eeb23f212798b5de43" },
+		getWeb3Path 		   : function() {  return "/usr/local/lib/node_modules/web3" },
+		
+};
