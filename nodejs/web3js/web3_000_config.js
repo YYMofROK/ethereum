f4961146e5296d262bb31185a19d4b37f4304fc5
YYMofROK <a84146943@gmail.com>
2020-11-11 16:49:38 +0900

    Update web3_000_config.js

--- nodejs/web3js/web3_000_config.js
+++ nodejs/web3js/web3_000_config.js
@@ -4,7 +4,10 @@
 
 module.exports = {
 		getNodeAddress_default : function() {  return "http://192.168.0.40:80" },
-//		getNodeAddress_default : function() {  return "https://mainnet.infura.io/v3/13f22305a33b4dfb987228667fd4806d" },
+	
+//	infra.io a84146943@gmail.com 으로 발급받은 APIKEY
+//	getNodeAddress_default : function() {  return "https://mainnet.infura.io/v3/13f22305a33b4dfb987228667fd4806d" },
+	
 		getWeb3Path 		   : function() {  return "/usr/local/lib/node_modules/web3" },
 		
 };
