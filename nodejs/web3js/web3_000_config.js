3d1236c8bce07a2b24f4a655cde608a297b15533
YYMofROK <a84146943@gmail.com>
2020-11-11 16:48:58 +0900

    Update web3_000_config.js

--- nodejs/web3js/web3_000_config.js
+++ nodejs/web3js/web3_000_config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
 		getNodeAddress_default : function() {  return "http://192.168.0.40:80" },
-//		getNodeAddress_default : function() {  return "https://mainnet.infura.io:443/v3/394a621745b743eeb23f212798b5de43" },
+//		getNodeAddress_default : function() {  return "https://mainnet.infura.io/v3/13f22305a33b4dfb987228667fd4806d" },
 		getWeb3Path 		   : function() {  return "/usr/local/lib/node_modules/web3" },
 		
 };
